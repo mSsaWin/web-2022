a00fbd258cc4af62202cd3c230eacd2f869b479c
Phil <xman1736@gmail.com>
2022-03-11 15:38:44 +0300

    Update header-buttons.js

--- Gnezdilov_F/js/header-buttons.js
+++ Gnezdilov_F/js/header-buttons.js
@@ -1,19 +1,15 @@
 advBtn = document.querySelector('.navbar-ad');
 profileBtn = document.querySelector('.navbar-profile');
-// advImg = document
 
 console.log(advBtn);
 
 advBtn.addEventListener('mouseover', () => {
     advBtn.style.width = '80%';
     advBtn.style.borderRadius = '25px';
-    // advBtn.setAttribute('class', 'btn btn-outline-success');
     advBtn.style.border = 'solid 0px #008CF0';
     advBtn.style.background = '#008CF0';
     profileBtn.style.display = 'none';
     advBtn.innerHTML = '<span class="hidden-text-ad">Разместить <wbr>объявление</span>';
-    // setInterval(() => {}, 300);
-
 });
 
 advBtn.addEventListener('mouseleave', () => {
@@ -29,12 +25,10 @@ advBtn.addEventListener('mouseleave', () => {
 profileBtn.addEventListener('mouseover', () => {
     profileBtn.style.width = '50%';
     profileBtn.style.borderRadius = '25px';
-    // profileBtn.setAttribute('class', 'btn btn-outline-success w=');
     profileBtn.style.border = 'solid 0px #00a550';
     profileBtn.style.background = '#00a550';
     advBtn.style.display = 'none';
     profileBtn.innerHTML = '<span class="hidden-text-ad">Войти</span>';
-    // setInterval(() => {}, 300);
 
 });
 
@@ -43,7 +37,6 @@ profileBtn.addEventListener('mouseleave', () => {
     profileBtn.style.borderRadius = '50%';
     profileBtn.style.border = 'solid 1px var(--color1)';
     profileBtn.style.background = '';
-    // advBtn.innerHTML = '';
     profileBtn.innerHTML = '<img id="navbar-profile-icon" src="../Gnezdilov_F/img/profile.svg" class="navbar-icons" alt="">';
     advBtn.style.display = 'flex';
 })
