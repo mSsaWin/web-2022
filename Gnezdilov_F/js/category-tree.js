d956dbc369ba4cd22aaba1edc3f326782e236dad
Phil <72728387+mSsaWin@users.noreply.github.com>
2022-03-14 13:02:57 +0300

    Настраиваю скрипт для категорий

--- Gnezdilov_F/js/category-tree.js
+++ Gnezdilov_F/js/category-tree.js
@@ -1,35 +1,50 @@
 tree = document.getElementById('tree');
-console.log(tree);
-for (let li of tree.querySelectorAll('li')) {
-    
+liTree = tree.querySelectorAll('li');
+for(let i = 0; i< liTree.length; i++){
     let span = document.createElement('span');
     span.classList.add('show');
-    li.prepend(span);
-    li.parentNode.hiden = true;
+    console.log();
+    liTree[i].prepend(span);
+    if (i !== 0) 
+        liTree[i].parentNode.hidden = true;
     span.append(span.nextSibling);
-
 }
 
-tree.onclick = (event)=>{
-    console.log(event.target.tagName);
-    console.log(event.target.parentNode);
-    if(event.target.tagName != 'SPAN') return;
 
+rightRow = document.createElement('i');
+rightRow.className = 'icon-right-open';
+
+downRow = document.createElement('i');
+downRow.className = 'icon-down-open';
+
+tree.onclick = (event)=>{
+    if(event.target.tagName != 'SPAN') return;
+    
     let childrenContainer = event.target.parentNode.querySelector('ul');
-    console.log(childrenContainer);
+    console.log(childrenContainer.hidden);
+    
     if(!childrenContainer) return;
     
     childrenContainer.hidden = !childrenContainer.hidden;
-    // if (childrenContainer.querySelector('a').length == 0){
-    //     if(childrenContainer.hidden){
-    //         event.target.classList.add('hide');
-    //         event.target.classList.remove('show');
-    //     }
-    //     else{
-    //         event.target.classList.add('show');
-    //         event.target.classList.remove('hide');
-    //     }
-    // }
+    
+    if(childrenContainer.hidden){
+        event.target.prepend(rightRow);
+        event.target.removeChild(downRow);
+    }
+    else{
+        event.target.prepend(downRow);
+        event.target.removeChild(rightRow);
+    }
+
+    if(childrenContainer.hidden){
+        event.target.classList.add('hide');
+        event.target.classList.remove('show');
+    }
+    else{
+        event.target.classList.add('show');
+        event.target.classList.remove('hide');
+    }
+    
     
 
 }
