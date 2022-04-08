a2620b0540c0f36fafc6fde1a1e38c16b1af0504
Phil <xman1736@gmail.com>
2022-04-08 17:29:36 +0300

    Да

--- Gnezdilov_F/frontend/src/components/Main.vue
+++ Gnezdilov_F/frontend/src/components/Main.vue
@@ -1,5 +1,32 @@
 <template>
-    
+    <div class="container">
+        <div class="row">
+                <div class="col-xl-3 col-md-4 col-sm-4 col-6 product-card">
+                    <div class="product">
+                        <div class="product-img">
+                            <a href="product.html"><img src="img/example_photo.jpg"></a>
+                        </div>
+                        <div class="product__content">
+                            <div class="product-title">
+                                <a href="product.html">Крутой задумчивый кот</a>
+                            </div>
+                            <div class="product-price">
+                                <a href="product.html">150 ₽</a>
+                                <div class="product-favorite">
+                                    <div class="product-favorite-circle">
+                                        <i class="icon-heart-empty"></i>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="product-publication">
+                                <a href="product.html">29 февраля, 2022</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
 </template>
 
 <script>
